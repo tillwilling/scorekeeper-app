c95db055a00246fb4df1bb775c8980bcdcbd918c
Till Willing <tillwilling@gmail.com>
2021-05-19 22:20:05 +0200

    fixed errors

--- src/components/History.stories.js
+++ src/components/History.stories.js
@@ -2,17 +2,17 @@ import react from 'react'
 import History from './History'
 
 export default {
-    title: 'History',
-    component: History
+  title: 'History',
+  component: History,
 }
 
 const Template = args => <History {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-    nameOfGame: 'Carcasonne'
-    players: [
-        {name: 'John Doe', score: 40},
-        {name: 'Jane Doe', score: 30},
-    ],
+  nameOfGame: 'Carcasonne',
+  players: [
+    { name: 'John Doe', score: 40 },
+    { name: 'Jane Doe', score: 30 },
+  ],
 }
--- src/components/Navigation.stories.js
+++ src/components/Navigation.stories.js
@@ -2,17 +2,17 @@ import React from 'react'
 import Navigation from './Navigation'
 
 export default {
-    title: 'Navigation'
-    component: Navigation
+  title: 'Navigation',
+  component: Navigation,
 }
 
 const Template = args => <Navigation {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-    currentPageId: 'play',
-    pages: [
-        {title: 'Play', id: 'play'},
-        {title: 'History', id: 'history'},
-    ],
+  currentPageId: 'play',
+  pages: [
+    { title: 'Play', id: 'play' },
+    { title: 'History', id: 'history' },
+  ],
 }
